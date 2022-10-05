b6088d796ca4a893e6abbe7bc360e257bf3b1145
Arturo Sanchez <thegamerslol2.zw@gmail.com>
2022-10-05 09:42:28 -0500

    se agrega el cuerpo del proyecto

---
+++ app.py
@@ -0,0 +1,17 @@
+lista_elements=[]
+
+
+def add_element():
+    pass
+def remove_element():
+    pass
+def show_element():
+    pass
+def show_elements():
+    pass
+def edit_element():
+    pass
+
+
+if __name__ == '__main__':
+    print("muestra el menu ")
