59283b1d42a01b2df63566446f78cac6699a0500
Arturo Sanchez <thegamerslol2.zw@gmail.com>
2022-10-05 09:49:16 -0500

    se agrega el menu

--- app.py
+++ app.py
@@ -14,4 +14,28 @@ def edit_element():
 
 
 if __name__ == '__main__':
-    print("muestra el menu ")
+     message=f"Tienda de abarrotes:\n  Elige la opcion\n 1=Buscar Producto\n  2=Editar Producto\n  3=Elimina Producto\n 4=Registrar Producto\n 5=Salir"
+     while True:
+        option=int(input(message))
+        #compara cada opcion y llama a la funcion correspondiente 
+        if option == 1:
+            #pedir numeros al usuario 
+            ##numeros=return_values()
+            
+        elif option== 2:
+            #pedir numeros al usuario 
+            ##numeros=return_values()
+            
+        elif option==3:
+            #pedir numeros al usuario
+            ##numeros=return_values() 
+           
+        elif option==4:
+            #pedir numeros al usuario
+            ##numeros=return_values() 
+            
+        elif option == 5:
+            print("BYE")
+            break
+        else:
+            print ("Opcion incorrecta")
